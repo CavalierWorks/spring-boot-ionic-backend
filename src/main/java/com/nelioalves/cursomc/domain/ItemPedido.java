6836cc424628a0d14bdef8f6e6d8d0abaed44fb2
Saulo Cavalier <cavalierworks@gmail.com>
2018-05-03 06:49:28 -0300

    Inserindo Pedido

--- src/main/java/com/nelioalves/cursomc/domain/ItemPedido.java
+++ src/main/java/com/nelioalves/cursomc/domain/ItemPedido.java
@@ -8,7 +8,7 @@ import javax.persistence.Entity;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
-public class ItemPedido implements Serializable {	
+public class ItemPedido implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@JsonIgnore
@@ -30,7 +30,7 @@ public class ItemPedido implements Serializable {
 		this.quantidade = quantidade;
 		this.preco = preco;
 	}
-	
+
 	public double getSubTotal() {
 		return (preco - desconto) * quantidade;
 	}
@@ -40,11 +40,18 @@ public class ItemPedido implements Serializable {
 		return id.getPedido();
 	}
 	
+	public void setPedido(Pedido pedido) {
+		id.setPedido(pedido);
+	}
 	
 	public Produto getProduto() {
 		return id.getProduto();
 	}
-
+	
+	public void setProduto(Produto produto) {
+		id.setProduto(produto);
+	}
+	
 	public ItemPedidoPK getId() {
 		return id;
 	}
@@ -100,8 +107,6 @@ public class ItemPedido implements Serializable {
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
-	}	
+	}
 	
-	
-
 }
--- src/main/java/com/nelioalves/cursomc/domain/PagamentoComCartao.java
+++ src/main/java/com/nelioalves/cursomc/domain/PagamentoComCartao.java
@@ -2,15 +2,17 @@ package com.nelioalves.cursomc.domain;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.nelioalves.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento {
 	private static final long serialVersionUID = 1L;
-	
+
 	private Integer numeroDeParcelas;
 	
-	public PagamentoComCartao() {		
+	public PagamentoComCartao() {
 	}
 
 	public PagamentoComCartao(Integer id, EstadoPagamento estado, Pedido pedido, Integer numeroDeParcelas) {
@@ -27,6 +29,5 @@ public class PagamentoComCartao extends Pagamento {
 	}
 	
 	
-	
-
+		
 }
